a86cdcd53c88b062f1dc8d0f3a257593e17a03db
GustavoFrota <gustavofro90@gmail.com>
2026-04-23 14:56:21 -0300

    Adicionando outro componente

---
+++ meu-projeto/src/Components/Frases.js
@@ -0,0 +1,7 @@
+function Frases() {
+    return (
+        <p>Outro componente.</p>
+    )
+}
+
+export default Frases;
--- meu-projeto/src/Components/HelloWorld.js
+++ meu-projeto/src/Components/HelloWorld.js
@@ -1,8 +1,11 @@
+import Frases from "./Frases"
+
 function HelloWorld() {
 
 
     return (
         <div>
+            <Frases/>
             <h1>Meu primeiro componente</h1>
         </div>
     )
