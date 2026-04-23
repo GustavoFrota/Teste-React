54a966a24784a8ef9acacdf9fc544a257705196b
GustavoFrota <gustavofro90@gmail.com>
2026-04-23 14:02:51 -0300

    Utilizando o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,14 @@
 import './App.css';
 
 function App() {
+const name = 'Gustavo'
+
+
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro app</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {name} </p>
     </div>
   );
 }
