b3a400f42be564e36c0c5fdbed7fc1896f5cbc05
GustavoFrota <gustavofro90@gmail.com>
2026-04-24 09:00:33 -0300

    Utilizando variáveis

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -5,11 +5,14 @@ import SayMyName from './Components/SayMyName';
 
 function App() {
 
+  const identidade = "Maria"
+
   return (
     <div className="App">
       <HelloWorld />
       <SayMyName nome="Gustavo" />
       <SayMyName nome="Roberto" />
+      <SayMyName nome={identidade} />
     </div>
   )
 }
