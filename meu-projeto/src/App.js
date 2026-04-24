f37cd51b670a7187751e65860e8c757fb352f68f
GustavoFrota <gustavofro90@gmail.com>
2026-04-24 09:17:03 -0300

    Adicionando props complexas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import HelloWorld from './Components/HelloWorld';
 import SayMyName from './Components/SayMyName';
+import Pessoa from './Components/Pessoa';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <SayMyName nome="Gustavo" />
       <SayMyName nome="Roberto" />
       <SayMyName nome={identidade} />
+      <Pessoa nome="Gustavo" idade="18" profissao="Programador" foto="htpps://via.placeholder.com/150" />
     </div>
   )
 }
