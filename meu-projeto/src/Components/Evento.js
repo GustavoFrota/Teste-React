fe5f8b0ea9b8bb6e3bf5606792230efeb101c0e1
GustavoFrota <gustavofro90@gmail.com>
2026-04-28 15:06:58 -0300

    Adicionano Segundo evento

--- meu-projeto/src/Components/Evento.js
+++ meu-projeto/src/Components/Evento.js
@@ -5,10 +5,15 @@ function Evento() {
         console.log("Primeiro evento ativado")
     }
 
+    function SegundoEvento() {
+        console.log("Segundo evento ativado")
+    }
+
     return (
         <>
         <p>Clique para ativar o evento:</p>
         <Button event={MeuEvento}  text="Primeiro evento"/>
+        <Button event={SegundoEvento}  text="Segundo evento"/>
         
         
         </>
