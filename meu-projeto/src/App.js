7d3efda3776f108bf31216a3ebe452e583d155a0
GustavoFrota <gustavofro90@gmail.com>
2026-05-04 09:48:45 -0300

    Adicionando outros caminhos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -13,6 +13,8 @@ import SeuNome from './Components/SeuNome';
 import Saudacao from './Components/Saudacao';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './Pages/Home';
+import Contato from './Pages/Contato';
+import Empresa from './Pages/Empresa';
 
 function App() {
 
@@ -42,6 +44,8 @@ function App() {
         </ul>
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/empresa' element={<Empresa />}></Route>
+          <Route path='/contato' element={<Contato />}></Route>
         </Routes>
       </Router>
     </div>
