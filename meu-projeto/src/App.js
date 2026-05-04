b51538a2b0ad8bcc50cdeef60c6e40ba5ed2c957
GustavoFrota <gustavofro90@gmail.com>
2026-05-04 09:17:51 -0300

    Adicionando outros links

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -36,6 +36,8 @@ function App() {
       <Router>
         <ul>
           <li> <Link to="/">Home</Link></li>
+          <li> <Link to="/empresa">Empresa</Link></li>
+          <li> <Link to="/contato">Contato</Link></li>
         </ul>
       </Router>
     </div>
