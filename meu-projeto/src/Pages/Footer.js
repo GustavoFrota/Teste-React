f6d0aff1286ecd740b8c2fd4af7d700865e884f0
GustavoFrota <gustavofro90@gmail.com>
2026-05-05 10:05:59 -0300

    Adicionando outros ícones

--- meu-projeto/src/Pages/Footer.js
+++ meu-projeto/src/Pages/Footer.js
@@ -1,9 +1,13 @@
-import { FaFacebook} from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
 
 function Footer() {
     return( 
         <footer>
-            <FaFacebook />
+            <ul>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
 
         </footer>
     )
